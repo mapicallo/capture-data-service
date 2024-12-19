71272bf6b592b0d97b3d2c42f2168fb195e84a01
mpicallo <mapicalloperez@hotmail.com>
2024-12-19 01:06:30 +0100

    initial structure

---
+++ src/main/java/com/example/capture_data_service/CaptureDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.capture_data_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptureDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptureDataServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/capture_data_service/CaptureDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capture_data_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptureDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
