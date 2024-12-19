5d45bd37f4a01e76c6aff37fb2625c3d175e3425
mpicallo <mapicalloperez@hotmail.com>
2024-12-19 03:36:29 +0100

    swagger up ok

---
+++ src/main/java/com/example/capture_data_service/api/isAlive.java
@@ -0,0 +1,25 @@
+package com.example.capture_data_service.api;
+
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@Tag(name = "Health Check", description = "Endpoints for health checking")
+public class isAlive {
+
+    @Operation(summary = "Check if the service is alive", description = "Returns 200 OK if the service is running")
+    @GetMapping("/isAlive")
+    public ResponseEntity<String> isAlive(){
+
+        return ResponseEntity.status(HttpStatus.OK).body("Service is running");
+    }
+
+}
